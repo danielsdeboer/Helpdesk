dd30e5505a2721c0d74e576bf97a575e69c79495
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2017-01-04 21:23:08 -0500

    Remove unique constraint from test user migration

--- database/migrations/2017_01_01_000000_create_users_table.php
+++ database/migrations/2017_01_01_000000_create_users_table.php
@@ -17,7 +17,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('email')->unique();
+            $table->string('email');
             $table->timestamps();
         });
 
