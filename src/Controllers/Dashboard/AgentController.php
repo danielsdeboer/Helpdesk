0fefd09fd403b5b2ba32f07ef5b1d52c93c78dd0
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2017-01-08 20:57:26 -0500

    Refactor controller methods to repository

--- src/Controllers/Dashboard/AgentController.php
+++ src/Controllers/Dashboard/AgentController.php
@@ -4,35 +4,12 @@ namespace Aviator\Helpdesk\Controllers\Dashboard;
 
 use Aviator\Helpdesk\Models\Agent;
 use Aviator\Helpdesk\Models\Ticket;
+use Aviator\Helpdesk\Repositories\Tickets;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\Auth;
 
 class AgentController extends Controller
 {
-    /**
-     * The agent
-     * @var Agent
-     */
-    protected $agent;
-
-    /**
-     * The agents teams tickets
-     * @var Collection
-     */
-    protected $teamTickets;
-
-    /**
-     * Overdue tickets for this agent or team
-     * @var Collection
-     */
-    protected $overdueTickets;
-
-    /**
-     * Open tickets assigned to agent
-     * @var Collection
-     */
-    protected $agentTickets;
-
     /**
      * Construct with agents only middleware
      */
@@ -47,77 +24,19 @@ class AgentController extends Controller
      */
     public function index()
     {
-        $this->agent = Agent::where('user_id', Auth::user()->id)->first();
-        $this->getTeamTickets()->getOverdueTickets()->getAgentTickets();
+        $agent = Agent::where('user_id', Auth::user()->id)->first();
 
         return [
-            'team' => $this->teamTickets,
-            'teamCount' => $this->teamTickets->count(),
+            'team' => Tickets::forAgent($agent)->team(),
+            'teamCount' => Tickets::forAgent($agent)->team()->count(),
 
-            'overdue' => $this->overdueTickets,
-            'overdueCount' => $this->overdueTickets->count(),
+            'overdue' => Tickets::forAgent($agent)->overdue(),
+            'overdueCount' => Tickets::forAgent($agent)->overdue()->count(),
 
-            'agent' => $this->agentTickets,
-            'agentCount' => $this->agentTickets->count(),
+            'agent' => Tickets::forAgent($agent)->all(),
+            'agentCount' => Tickets::forAgent($agent)->all()->count(),
 
             'tab' => 'dashboard',
         ];
-    }
-
-    /**
-     * Get the tickets for the agents team
-     * @return $this
-     */
-    protected function getTeamTickets()
-    {
-        $this->teamTickets = Ticket::with('user', 'opening')
-            ->whereHas('poolAssignment', function($query) {
-                $query->whereIn('pool_id', $this->agent->teams->pluck('id'));
-            })
-            ->unassigned()
-            ->get();
-
-        return $this;
-    }
-
-    /**
-     * Get overdue tickets for this agent or agents team
-     * @return $this
-     */
-    protected function getOverdueTickets()
-    {
-        $this->overdueTickets = Ticket::with('user', 'dueDate')
-            ->whereHas('assignment', function($query) {
-                $query->where('assigned_to', $this->agent->id);
-            })
-            ->orWhereHas('poolAssignment', function($query) {
-                $query->whereIn('pool_id', $this->agent->teams->pluck('id'));
-            })
-            ->overdue()
-            ->get()
-            ->sortBy(function($item) {
-                return $item->dueDate->due_on;
-            });
-
-        return $this;
-    }
-
-    /**
-     * Get all open tickets assigned directly to the agent
-     * @return $this
-     */
-    protected function getAgentTickets()
-    {
-        $this->agentTickets = Ticket::with('user')
-            ->whereHas('assignment', function($query) {
-                $query->where('assigned_to', $this->agent->id);
-            })
-            ->opened()
-            ->get()
-            ->sortBy(function($item) {
-                return isset($item->dueDate->due_on) ? $item->dueDate->due_on->toDateString() : 9999999;
-            });
-
-        return $this;
     }
 }
--- src/Repositories/Tickets.php
+++ src/Repositories/Tickets.php
@@ -3,6 +3,7 @@
 namespace Aviator\Helpdesk\Repositories;
 
 use Aviator\Helpdesk\Models\Agent;
+use Aviator\Helpdesk\Models\Ticket;
 
 class Tickets
 {
@@ -19,31 +20,53 @@ class Tickets
     protected $agent;
 
     /**
-     * Constructor
-     * @param User | Agent $user
+     * Static constructor with agent
+     * @param  Agent $agent
+     * @return Tickets
      */
-    public function __construct($userOrAgent)
+    public static function forAgent($agent)
+    {
+        return (new self)->setAgent($agent);
+    }
+
+    /**
+     * Static constructor with user
+     * @param  mixed $agent
+     * @return Tickets
+     */
+    public static function forUser($user)
+    {
+        return (new self)->setUser($user);
+    }
+
+    /**
+     * Set the agent
+     * @param Agent $agent
+     */
+    public function setAgent(Agent $agent)
+    {
+        $this->agent = $agent;
+        $this->setUser($agent->user);
+
+        return $this;
+    }
+
+    /**
+     * Set the user
+     * @param mixed $user
+     */
+    public function setUser($user)
     {
         $userModel = config('helpdesk.userModel');
 
         if ($user instanceof $userModel) {
-            $this->user = $userOrAgent;
-        } elseif ($user instanceof Agent) {
-            $this->user = $userOrAgent->user;
-            $this->agent = $userOrAgent;
+            $this->user = $user;
         } else {
-            throw new Exception('You must provide an instance of User or Agent');
-        }
-    }
+            throw new \Exception('You must provide an instance of '. $userModel);
 
-    /**
-     * Static constructor
-     * @param  User | Agent $userOrAgent
-     * @return Tickets
-     */
-    public function owner($userOrAgent)
-    {
-        return new self($userOrAgent);
+        }
+
+        return $this;
     }
 
     /**
@@ -52,12 +75,57 @@ class Tickets
      */
     public function team()
     {
+        if (! $this->agent) {
+            return null;
+        }
+
         return Ticket::with('user', 'opening')
             ->whereHas('poolAssignment', function($query) {
                 $query->whereIn('pool_id', $this->teamIds());
             })
             ->unassigned()
             ->get();
+    }
+
+    /**
+     * Return overdue tickets
+     * @return Collection
+     */
+    public function overdue()
+    {
+        if ($this->agent) {
+            return Ticket::with('user', 'dueDate')
+                ->whereHas('assignment', function($query) {
+                    $query->where('assigned_to', $this->agent->id);
+                })
+                ->orWhereHas('poolAssignment', function($query) {
+                    $query->whereIn('pool_id', $this->teamIds());
+                })
+                ->overdue()
+                ->get()
+                ->sortBy(function($item) {
+                    return $item->dueDate->due_on;
+                });
+        }
+    }
+
+    /**
+     * Return all open tickets
+     * @return Collection
+     */
+    public function all()
+    {
+        if ($this->agent) {
+            return Ticket::with('user')
+                ->whereHas('assignment', function($query) {
+                    $query->where('assigned_to', $this->agent->id);
+                })
+                ->opened()
+                ->get()
+                ->sortBy(function($item) {
+                    return isset($item->dueDate->due_on) ? $item->dueDate->due_on->toDateString() : 9999999;
+                });
+        }
     }
 
     /**
