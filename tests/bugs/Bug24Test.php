1ed04fe065bcec3f980aec2ddb075b323dc4d7b6
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2017-06-10 13:12:24 -0400

    Fix #24 (#26)
    * Display ticket title at top of page, fix #24
    
    * Remove unneeded uses

---
+++ tests/bugs/Bug24Test.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Aviator\Helpdesk\Tests;
+
+use Aviator\Helpdesk\Models\Ticket;
+
+class Bug24Test extends AdminBase
+{
+    const PUB = 'helpdesk/tickets/public/';
+
+    /**
+     * @group bugs
+     * @group bugs.24
+     * @test
+     */
+    public function ticketShouldShowTheTicketName()
+    {
+        $ticket = factory(Ticket::class)->create();
+
+        $this->visit(self::PUB . $ticket->uuid)
+            ->see('<p class="title">' . $ticket->content->title() . '</p>');
+    }
+}
