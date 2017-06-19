bccbd24ae3d8d23af91463fccbafea25bc23bd57
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2017-06-19 11:28:57 -0400

    Add tests

--- src/Queries/QueryInterface.php
+++ src/Queries/QueryInterface.php
@@ -5,5 +5,6 @@ namespace Aviator\Helpdesk\Queries;
 interface QueryInterface
 {
     public static function make(...$args);
+    public static function builder(...$args);
     public function query();
 }
--- tests/User.php
+++ tests/User.php
@@ -2,11 +2,12 @@
 
 namespace Aviator\Helpdesk\Tests;
 
+use Aviator\Helpdesk\Models\Ticket;
 use Illuminate\Auth\Authenticatable;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\Access\Authorizable;
-use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
-use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 
 class User extends Model implements AuthorizableContract, AuthenticatableContract
 {
@@ -24,4 +25,9 @@ class User extends Model implements AuthorizableContract, AuthenticatableContrac
     public $timestamps = false;
 
     protected $table = 'users';
+
+    public function tickets()
+    {
+        return $this->hasMany(Ticket::class);
+    }
 }
