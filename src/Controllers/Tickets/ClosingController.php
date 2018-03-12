e6a78f219fc6df8f24fa3b87ec3b888993591d5f
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-03-12 13:48:27 -0400

    Apply fixes from StyleCI (#90)

--- src/Controllers/Tickets/ClosingController.php
+++ src/Controllers/Tickets/ClosingController.php
@@ -23,7 +23,7 @@ class ClosingController extends Controller
      */
     protected function create(Request $request, Ticket $ticket)
     {
-        if (! $ticket) {
+        if (!$ticket) {
             return redirect()->route('helpdesk.tickets.index');
         }
 
