907f63749a70fd468c74b4ca154b49474adbf52c
Daniel S. Deboer <daniel.s.deboer@gmail.com>
2018-03-13 15:04:28 -0400

    Add modal component

---
+++ resources/views/tickets/show/toolbar/script.blade.php
@@ -0,0 +1,100 @@
+@if(config('app.debug'))
+  <script src="https://unpkg.com/vue/dist/vue.js"></script>
+@else
+  <script src="https://unpkg.com/vue/dist/vue.min.js"></script>
+@endif
+
+<script>
+  /*
+   * Use ES-past syntax for wider compatibility.
+   */
+  Vue.component('form-modal', {
+    props: ['modalName', 'modalTitle', 'actionRoute', 'csrfToken', 'availableModals', 'buttonText'],
+
+    computed: {
+      isVisible: function () {
+        return this.availableModals[this.modalName].visible
+      }
+    },
+
+    methods: {
+      close: function () {
+        this.$emit('close-modal', {modalName: this.modalName})
+      }
+    },
+
+    mounted: function () {
+      document.addEventListener("keydown", (e) => {
+        if (this.isVisible && e.keyCode == 27) {
+          this.close();
+        }
+      })
+    },
+
+    template: `
+      <div
+        class="modal"
+        v-bind:class="{
+          'is-active': isVisible
+        }"
+        v-on:keydown.esc="close"
+      >
+        <div class="modal-background" @click="close"></div>
+
+        <div class="modal-content">
+          <div class="box">
+            <h1 class="title" v-text="modalTitle"></h1>
+
+            <form method="post" :action="actionRoute">
+              <input type="hidden" name="_token" :value="csrfToken">
+
+              <slot></slot>
+
+              <div class="control is-grouped">
+                <p class="control">
+                  <button class="button is-primary" name="close_submit" v-text="buttonText"></button>
+                </p>
+
+                <p class="control">
+                  <button class="button is-link" @click.prevent="close">Cancel</button>
+                </p>
+              </div>
+            </form>
+          </div>
+        </div>
+
+        <button class="modal-close" @click="close"></button>
+      </div>
+    `,
+  })
+
+  var app = new Vue({
+    el: '.modal-app',
+
+    data: {
+      modals: {
+        close: {
+          visible: false
+        },
+        open: {
+          visible: false
+        },
+        reply: {
+          visible: false
+        }
+      }
+    },
+
+    methods: {
+      toggle: function (modal) {
+        this.modals[modal].visible = ! this.modals[modal].visible
+      },
+
+      close: function ({modalName}) {
+        if (this.modals[modalName].visible) {
+          this.toggle(modalName)
+        }
+      }
+    }
+  });
+</script>
--- resources/views/tickets/show/user-toolbar.blade.php
+++ resources/views/tickets/show/user-toolbar.blade.php
@@ -21,149 +21,49 @@
     ])
   @endif
 
-  {{-- CLOSE A TICKET --}}
-  <div
-    class="modal"
-    v-bind:class="{
-      'is-active': modals.close.visible
-    }"
-    v-if="modals.close.visible"
-    v-on:keydown.esc="close('close')"
+  {{-- Ticket Closing --}}
+
+  <form-modal
+    modal-name="close"
+    modal-title="Close This Ticket"
+    action-route="{{ route('helpdesk.tickets.close', $ticket->id) }}"
+    csrf-token="{{ csrf_token() }}"
+    button-text="Close Ticket"
+    :available-modals="modals"
+    @close-modal="close"
   >
-    <div class="modal-background" @click="toggle('close')"></div>
-    <div class="modal-content">
-      <div class="box">
-        <h1 class="title">Close This Ticket</h1>
+    <p class="control">
+      <textarea name="note" class="textarea" placeholder="Closing note (optional)"></textarea>
+    </p>
+  </form-modal>
 
-        <form method="post" action="{{ route('helpdesk.tickets.close', $ticket->id) }}">
-          {{ csrf_field() }}
-
-          <p class="control">
-            <textarea name="note" class="textarea" placeholder="Closing note (optional)"></textarea>
-          </p>
-
-          <div class="control is-grouped">
-            <p class="control">
-              <button class="button is-primary" name="close_submit">Close Ticket</button>
-            </p>
-
-            <p class="control">
-              <button class="button is-link" @click.prevent="toggle('close')">Cancel</button>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-    <button class="modal-close" @click="toggle('close')"></button>
-  </div>
-
-  {{-- REPLY TO A TICKET --}}
-  <div
-    class="modal"
-    v-bind:class="{
-      'is-active': modals.reply.visible
-    }"
-    v-if="modals.reply.visible"
-    v-on:keydown.esc="close('reply')"
+  <form-modal
+    modal-name="open"
+    modal-title="Reopen This Ticket"
+    action-route="{{ route('helpdesk.tickets.open', $ticket->id) }}"
+    csrf-token="{{ csrf_token() }}"
+    button-text="Open Ticket"
+    :available-modals="modals"
+    @close-modal="close"
   >
-    <div class="modal-background" @click="toggle('reply')"></div>
-    <div class="modal-content">
-      <div class="box">
-        <h1 class="title">Reply To This Ticket</h1>
+    <p class="control">
+      <textarea name="note" class="textarea" placeholder="Note (optional)"></textarea>
+    </p>
+  </form-modal>
 
-        <form method="post" action="{{ route('helpdesk.tickets.reply', $ticket->id) }}">
-          {{ csrf_field() }}
-
-          <p class="control">
-            <textarea name="reply_body" class="textarea" placeholder="Reply Body"></textarea>
-          </p>
-
-          <div class="control is-grouped">
-            <p class="control">
-              <button class="button is-primary" name="reply_submit">Reply To Ticket</button>
-            </p>
-
-            <p class="control">
-              <button class="button is-link" @click.prevent="toggle('reply')">Cancel</button>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-    <button class="modal-close" @click="toggle('reply')"></button>
-  </div>
-
-  {{-- OPEN A TICKET --}}
-  <div
-    class="modal"
-    v-bind:class="{
-      'is-active': modals.open.visible
-    }"
-    v-if="modals.open.visible"
-    v-on:keydown.esc="close('open')"
+  <form-modal
+    modal-name="reply"
+    modal-title="Add a Reply"
+    action-route="{{ route('helpdesk.tickets.reply', $ticket->id) }}"
+    csrf-token="{{ csrf_token() }}"
+    button-text="Add Reply"
+    :available-modals="modals"
+    @close-modal="close"
   >
-    <div class="modal-background" @click="toggle('open')"></div>
-    <div class="modal-content">
-      <div class="box">
-        <h1 class="title">Open This Ticket</h1>
-
-        <form method="post" action="{{ route('helpdesk.tickets.open', $ticket->id) }}">
-          {{ csrf_field() }}
-
-          <p class="control">
-            <textarea name="note" class="textarea" placeholder="Note (optional)"></textarea>
-          </p>
-
-          <div class="control is-grouped">
-            <p class="control">
-              <button class="button is-primary" name="open_submit">Open Ticket</button>
-            </p>
-
-            <p class="control">
-              <button class="button is-link" @click.prevent="toggle('open')">Cancel</button>
-            </p>
-          </div>
-        </form>
-      </div>
-    </div>
-    <button class="modal-close" @click="toggle('open')"></button>
-  </div>
+    <p class="control">
+      <textarea name="reply_body" class="textarea" placeholder="Reply Body"></textarea>
+    </p>
+  </form-modal>
 </div>
 
-@if(config('app.debug'))
-  <script src="https://unpkg.com/vue/dist/vue.js"></script>
-@else
-  <script src="https://unpkg.com/vue/dist/vue.min.js"></script>
-@endif
-
-<script>
-  var app = new Vue({
-    el: '.modal-app',
-
-    data: {
-      modals: {
-        close: {
-          visible: false
-        },
-        open: {
-          visible: false
-        },
-        reply: {
-          visible: false
-        }
-      }
-    },
-
-    methods: {
-      toggle: function (modal) {
-        this.modals[modal].visible = ! this.modals[modal].visible
-      },
-
-      close: function (modal) {
-        if (this.modals[modal].visible) {
-            this.toggle(modal)
-        }
-      }
-    }
-  });
-</script>
+@include('helpdesk::tickets.show.toolbar.script')
