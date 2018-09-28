54b2222e3f92935d1b4604bb5af38c3960e5e27d
LeviXIII <matthewthomas13@hotmail.com>
2018-09-28 11:26:53 -0400

    Add IgnoredTicketController with views and tests.

---
+++ resources/views/tickets/ignored/index.blade.php
@@ -0,0 +1,21 @@
+@extends('helpdesk::layout.main')
+
+@section('content')
+  <div class="section">
+    <div class="container">
+      <h1 class="title">Ignored Tickets</h1>
+
+      {{ $ignored->links('helpdesk::pagination.bulma') }}
+
+      @if (auth()->user()->agent->is_super)
+        @include('helpdesk::tickets.ignored.index.table-super', [
+          'tickets' => $ignored
+        ])
+      @else
+        <p>Nothing to see here.</p>
+      @endif
+
+      {{ $ignored->links('helpdesk::pagination.bulma') }}
+    </div>
+  </div>
+@endsection
